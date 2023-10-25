974950d09b7cc166b574e14501c323f295d5d24e
anexdev1 <anwarnasir0970@gmail.com>
2023-10-25 11:37:01 +0300

    implemented creating order

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
@@ -27,8 +29,9 @@ const router = createBrowserRouter([
         element: <Cart />,
       },
       {
-        path: "/order.new",
+        path: "/order/new",
         element: <CreateOrder />,
+        action: createOrderAction,
       },
       {
         path: "/order/:orderID",
