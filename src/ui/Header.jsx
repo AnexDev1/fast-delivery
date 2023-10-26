f7569a6d1c68aaa7b525f762f1f6f39e6f5f6c0d
anexdev1 <anwarnasir0970@gmail.com>
2023-10-26 13:45:08 +0300

    configured tailwind and prettier

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,8 +3,10 @@ import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast Delivery Co.</Link>
+    <header className="">
+      <Link to="/" className="">
+        Fast Delivery Co.
+      </Link>
       <SearchOrder />
       <p>Anwar</p>
     </header>
